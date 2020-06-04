2df08d03af620389cf755dd0e47184d0cbb368c8
iggster <ignatius.murillo@gmail.com>
2020-06-04 15:26:24 -0400

    Addes user router boiler plate code

---
+++ src/routers/users.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = new express.Router()
+
+
+//Delete user.
+router.delete('/users', async (req,res) => {
+    res.status(200).send('Deleting user.')
+}) 
+
+
+//Get user profile.
+router.get('/users/me', async (req,res) => {
+    res.status(200).send('Get user profile.')
+})
+
+
+//Update user.
+router.patch('/users', async (req,res) => {
+    res.status(200).send('Updating user.')
+})
+
+//Create user
+router.post('/users', async (req,res) => {
+    res.status(200).send('Create user.')
+})
+
+//User login.
+router.post('/users/login', async (req, res) => {
+    res.status(200).send('Logging in user.')
+})
+
+//User logout.
+router.post('/users/logout', async (req,res) => {
+    res.status(200).send('Logging out user.')
+})
+
+//User logout all sessions.
+router.post('/users/logoutALL', async (req,res) => {
+    res.status(200).send('Logging out all sessions.')
+})
+
+
