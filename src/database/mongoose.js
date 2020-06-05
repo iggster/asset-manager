a7d51a31f5cc65de39a0c45d37c2730cad66d09a
iggster <ignatius.murillo@gmail.com>
2020-06-05 12:59:53 -0400

    Fixed Mongo connection string error.

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const config = require('config')
-const mongoDbURI = config.get('MONGO_ATLAS_DB')
 const assetDB = process.env.MONGO_DB_CONNECTION_ASSET_MANAGER
 
-mongoose.connect(mongoDbURI, {
+mongoose.connect(assetDB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
