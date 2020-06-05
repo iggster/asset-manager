0fead4128f2f370470f357ae08caec258215fd6a
iggster <ignatius.murillo@gmail.com>
2020-06-05 13:14:45 -0400

    Added comment to Mongoose connection.

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const assetDB = process.env.MONGO_DB_CONNECTION_ASSET_MANAGER
 
+
+//Monggose connect to Mongo.
 mongoose.connect(assetDB, {
     useNewUrlParser: true,
     useCreateIndex: true,
